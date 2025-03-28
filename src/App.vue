e04dae0c70c19b8919b0a2dfa37929ace85149ab
gaSombrio <sombrio.ga@gmail.com>
2025-03-28 11:32:30 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const Contador = ref(0)
+
+  function incrementar() {
+    Contador.value++
+  }
+  function decrementar() {
+    Contador.value--
+  }
+</script>
+
+<template>
+    <h1>Contador</h1>
+    <p>Valor do Contador: {{ Contador }}</p>
+    <button v-on:click="incrementar">+</button>
+    <button @click="decrementar">-</button>
+</template>
+
+<style scoped>
+
+
+
+</style>
